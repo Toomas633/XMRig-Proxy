5d3f96555e7085e0f79527cbb0c0c90fc2349e47
Toomas <toomas.kirsing@gmail.com>
2024-06-13 18:20:15 +0300

    Remove XMRig from mock docker image

--- entrypoint_mock.sh
+++ entrypoint_mock.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-apache2ctl -D FOREGROUND &
-
 python3 /mock/mock.py &
 
-XMRIG_PROXY_OPTIONS="-o $POOL_URL -u $USER -p $PASS --algo $ALGO --mode simple --http-host 0.0.0.0 --http-port 8081"
-
-/xmrig-proxy/build/xmrig-proxy $XMRIG_PROXY_OPTIONS
+apache2ctl -D FOREGROUND
